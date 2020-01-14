b75aee299499545c4855a5e96bb53e96d1562985
bzuniga <bzuniga@remeco.net>
2020-01-13 21:08:46 -0500

    initial commit

---
+++ src/main/java/com/restaurante/proyecto/Proyectosoftware2Application.java
@@ -0,0 +1,24 @@
+package com.restaurante.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Proyectosoftware2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyectosoftware2Application.class, args);
+    }
+
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    String hello() {
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/com/restaurante/proyecto/Proyectosoftware2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectosoftware2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
