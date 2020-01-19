84e42aa754e8143244d11a46555d6bf0591fe4b0
Bryan Zúñiga <bryan.zuniga@udla.edu.ec>
2020-01-19 05:43:17 -0500

    Revert "prueba correción error"
    This reverts commit b0a8037e74be4d39baa0ef24719331956a43898b.

--- src/main/java/com/restaurante/proyecto/Proyectosoftware2Application.java
+++ src/main/java/com/restaurante/proyecto/Proyectosoftware2Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -80,7 +79,6 @@ class Greeting {
         this.message = message;
     }
 
-    @Repository
     interface GreetingRepository extends CrudRepository<Greeting, Long> {
     }
 }
