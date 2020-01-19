618606633e55b4b00b4410b7bae4641b2a584c37
Bryan Zúñiga <bryan.zuniga@udla.edu.ec>
2020-01-19 14:57:25 -0500

    Revert "Merge pull request #1 from bzuniga03/Bryan"
    This reverts commit 9e9974fe2b8e552f3703957a7de42284fee37021, reversing
    changes made to eb92ed36c39d5c4606a9e358a2b90c7b76e49a69.

--- src/main/java/com/restaurante/proyecto/Proyectosoftware2Application.java
+++ src/main/java/com/restaurante/proyecto/Proyectosoftware2Application.java
@@ -1,15 +1,9 @@
 package com.restaurante.proyecto;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.*;
-import java.util.List;
 
 @SpringBootApplication
 public class Proyectosoftware2Application {
@@ -18,67 +12,13 @@ public class Proyectosoftware2Application {
         SpringApplication.run(Proyectosoftware2Application.class, args);
     }
 
-    @Bean
-    ApplicationRunner applicationRunner(Greeting.GreetingRepository greetingRepository) {
-        return args -> {
-            greetingRepository.save(new Greeting("Hola"));
-            greetingRepository.save(new Greeting("Hi"));
-        };
-
-    }
 }
 
 @RestController
 class HelloController {
-    private final Greeting.GreetingRepository greetingRepository;
-
-    HelloController(Greeting.GreetingRepository greetingRepository) {
-        this.greetingRepository = greetingRepository;
-    }
-
     @GetMapping("/")
     String hello() {
         return "Hello World";
     }
 
-    @GetMapping("/greetings")
-    List<Greeting> greetings() {
-        return (List<Greeting>) greetingRepository.findAll();
-    }
-
-}
-
-@Entity
-class Greeting {
-    @Id
-    @GeneratedValue
-    private Long id;
-    @Column
-    private String message;
-
-    public Greeting() {
-    }
-
-    public Greeting(String message) {
-        this.message = message;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    interface GreetingRepository extends CrudRepository<Greeting, Long> {
-    }
 }
