bf4ef99a9ea3781b7828f663aec3f073a20b8eee
Bryan Zúñiga <bryan.zuniga@udla.edu.ec>
2020-01-19 05:17:46 -0500

    Revert "prueba"
    This reverts commit d4dc049fbe2a3858eb6dc18b0fb416117941906b.

--- src/main/java/com/restaurante/proyecto/Proyectosoftware2Application.java
+++ src/main/java/com/restaurante/proyecto/Proyectosoftware2Application.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.*;
+import java.util.List;
 
 @SpringBootApplication
 public class Proyectosoftware2Application {
@@ -41,8 +42,8 @@ class HelloController {
     }
 
     @GetMapping("/greetings")
-    Iterable<Greeting> greetings() {
-        return greetingRepository.findAll();
+    List<Greeting> greetings() {
+        return (List<Greeting>) greetingRepository.findAll();
     }
 
 }
